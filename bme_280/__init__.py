2f189818654c812a867e6b03cdf566fcb12bac5a
AixNPanes <timdaley@earthling.net>
2025-06-09 09:43:04 -0400

    make BME_280 subclass SMBusDevice

--- bme_280/__init__.py
+++ bme_280/__init__.py
@@ -4,13 +4,40 @@ from typing import Any, Dict
 import bme280
 from smbus2 import SMBus
 
-class BME_280(SMBus):
+class SMBusDevice(SMBus):
     bus:int = None
     address = None
+    last_update:datetime.datetime = datetime.datetime.now()
+
+    def __ini__(self, bus:int = 1, address:int = 0x76):
+       super().__init__(bus)
+       self.bus = bus
+       self.address = address
+
+    # Override this method
+    def sample(self) -> None:
+        self.last_update = datetime.datetime.now()
+
+    # Override this method
+    def data(self) -> Dict[str, Any]:
+        return {
+                "last_update": self.last_update.strftime('%m/%d/%Y %H:%M:%S'),
+                "bus": self.bus,
+                "address": self.address,
+                }
+
+    # Override this method if desired
+    def toJson() -> str:
+        return ""
+
+    # Override this method if desired
+    def __str__(self) -> str:
+        return f"bus: {self.bus}, address: {self.address}"
+
+class BME_280(SMBusDevice):
     temperature:float = -32.0 * 5 / 9
     pressure:float = 0.0
     humidity:float = 0.0
-    last_update:datetime.datetime = datetime.datetime.now()
 
     def __init__(self, bus:int = 1, address:int = 0x76):
         super().__init__(bus)
@@ -19,11 +46,11 @@ class BME_280(SMBus):
         self._calibration_params = bme280.load_calibration_params(self, self.address)
 
     def sample(self) -> None:
+        super().sample()
         data = bme280.sample(self, self.address, self._calibration_params)
         self.temperature = data.temperature
         self.pressure = data.pressure
         self.humidity = data.humidity
-        self.last_update = datetime.datetime.now()
 
     def data(self) -> Dict[str, Any]:
         return {
@@ -35,15 +62,10 @@ class BME_280(SMBus):
                 "humidity": round(self.humidity, 1),
                 }
 
-    def toJson() -> str:
-        return ""
-
-    def __str__(self) -> str:
-        return f"busx: {self.bus}, address: {self.address}"
-
 if __name__ == '__main__':
     import json
     bme = BME_280()
+    print(bme)
     bme.sample()
-    print(json.dumps(bme, indent=4))
+    print(bme.data())
 
