a4d0e6ab0290d5a4c91046639706178a6f3a590e
AixNPanes <timdaley@earthling.net>
2025-06-11 19:32:55 -0400

    comment local logging module

--- hamqtt_logging/__init__.py
+++ hamqtt_logging/__init__.py
@@ -2,6 +2,23 @@ import logging
 import flask.logging
 
 def loggerConfig(logginglevel:int):
+    """ logging configuration
+
+    Parameters
+    ----------
+
+    logginglevel : int
+
+        The levels are:
+            50 : CRITICAL
+            50 : FATAL
+            40 : ERROR
+            30 : WARN
+            20 : INFO
+            10 : DEBUG
+            0 : NOTSET
+
+    """
     logging.basicConfig(
         level=logginglevel,
         format='[%(asctime)s] - %(levelname)s in %(name)s: %(message)s',
